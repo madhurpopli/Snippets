74f660ed4f6dfc2a77591449dbab14d1fa5d20bb
madhurpopli <32796635+madhurpopli@users.noreply.github.com>
2020-01-08 16:10:14 +0530

    Update access_S3_boto.py

--- AWS/access_S3_boto.py
+++ AWS/access_S3_boto.py
@@ -18,3 +18,12 @@ obj = s3.get_object(Bucket= bucket, Key= file_name)
 # get object and file (key) from bucket
 
 initial_df = pd.read_csv(obj['Body'])
+
+
+def downloadDirectoryFroms3(bucketName,remoteDirectoryName):
+    s3_resource = session.resource('s3')
+    bucket = s3_resource.Bucket(bucketName) 
+    for object in bucket.objects.filter(Prefix = remoteDirectoryName):
+        if not os.path.exists(os.path.dirname(object.key)):
+            os.makedirs(os.path.dirname(object.key))
+        bucket.download_file(object.key,object.key)
